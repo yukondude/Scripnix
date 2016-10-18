3ce59e7eba3470a24fee55362d6b14296fc78e6d
Dave Rogers <thedude@yukondude.com>
2016-10-18 13:21:06 -0700

    Flesh out the logic of the hyphenate command.

--- scripnix/pycommand/hyphenate.py
+++ scripnix/pycommand/hyphenate.py
@@ -5,20 +5,32 @@
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
 import click
+import re
 import sys
 from .command import common_command_and_options
 
 
 COMMAND_NAME = "hyphenate"
 
+# Matches one or more non-word (not including underscore or period) characters.
+NON_WORD_REGEX = re.compile(r"[^\w.]+")
+
+
+def hyphenate(lines, delimiter):
+    """ Return the list of lines with all non-word characters replaced by the given delimiter.
+    """
+    # Replace non-word characters by splitting the string at sequences of them and then joining the non-empty ones back using the delimiter.
+    return [delimiter.join(w for w in NON_WORD_REGEX.split(l) if w) for l in lines]
+
 
 @common_command_and_options(command_name=COMMAND_NAME, add_dry_run=False)
-def main():
-    """ Translate the given filenames (via stdin) into their equivalent, filesystem-safe, hyphenated versions.
+@click.option('--delimiter', '-d', default='-', show_default=True, help="Word delimiter character(s).")
+def main(delimiter):
+    """ Translate the given input (via STDIN) into its equivalent, filesystem-safe, hyphenated version.
 
         The hyphenate command is part of Scripnix.
     """
-    for line in sys.stdin:
+    for line in hyphenate(lines=sys.stdin, delimiter=delimiter):
         click.echo(line.strip())
 
 
