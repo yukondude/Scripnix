1a52797f362dc1cc68f36d81ab7f093a79509d2d
Dave Rogers <thedude@yukondude.com>
2016-10-18 21:11:18 -0700

    Allow command line arguments to be hyphenated too.

--- scripnix/pycommand/hyphenate.py
+++ scripnix/pycommand/hyphenate.py
@@ -19,18 +19,25 @@ NON_WORD_REGEX = re.compile(r"[^\w.]+")
 def hyphenate(lines, delimiter):
     """ Return the list of lines with all non-word characters replaced by the given delimiter.
     """
-    # Replace non-word characters by splitting the string at sequences of them and then joining the non-empty ones back using the delimiter.
+    # Replace non-word characters by splitting the string at sequences of them and then joining the non-empty fragments together using the
+    # delimiter.
     return [delimiter.join(w for w in NON_WORD_REGEX.split(l) if w) for l in lines]
 
 
 @common_command_and_options(command_name=COMMAND_NAME)
 @click.option('--delimiter', '-d', default='-', show_default=True, help="Word delimiter character(s).")
-def main(delimiter):
-    """ Translate the given input (via STDIN) into its equivalent, filesystem-safe, hyphenated version.
+@click.argument('text', nargs=-1)
+def main(delimiter, text):
+    """ Translate the given text argument(s) (or use the input lines from STDIN) into the equivalent, filesystem-safe, hyphenated versions.
 
         The hyphenate command is part of Scripnix.
     """
-    for line in hyphenate(lines=sys.stdin, delimiter=delimiter):
+    if text:
+        lines = text
+    else:
+        lines = sys.stdin
+
+    for line in hyphenate(lines=lines, delimiter=delimiter):
         click.echo(line.strip())
 
 
