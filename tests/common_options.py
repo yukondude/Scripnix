aefa4ab62c570bcc2e074b42f70167b203cdfdc9
Dave Rogers <thedude@yukondude.com>
2016-10-19 14:16:30 -0700

    Pre-compile regexs in case it speeds things up just a smidge.

--- tests/common_options.py
+++ tests/common_options.py
@@ -9,17 +9,23 @@ import re
 from scripnix import __version__
 
 
+HELP_OPTION_REGEX = re.compile(r"Usage:.+The (\S+) command.+Scripnix\..+Show this message and exit\.$")
+
+
 def common_help_option(command_entry, command_name):
     """ Test help message for both standard option switches."""
     def version_option_switch(option_switch):
         runner = CliRunner()
         result = runner.invoke(command_entry, [option_switch])
         assert result.exit_code == 0
-        match = re.match(r"Usage:.+The (\S+) command.+Scripnix\..+Show this message and exit\.$", result.output.replace("\n", ""))
+        match = HELP_OPTION_REGEX.match(result.output.replace("\n", ""))
         assert match.group(1) == command_name
 
     version_option_switch("-h")
     version_option_switch("--help")
+
+
+VERSION_OPTION_REGEX = re.compile(r"The (\S+).+Scripnix.+(\d+\.\d+\.\d+)\.Copyright.+\.$")
 
 
 def common_version_option(command_entry, command_name):
@@ -28,7 +34,7 @@ def common_version_option(command_entry, command_name):
         runner = CliRunner()
         result = runner.invoke(command_entry, [option_switch])
         assert result.exit_code == 0
-        match = re.match(r"The (\S+).+Scripnix.+(\d+\.\d+\.\d+)\.\nCopyright.+\.\n$", result.output, re.MULTILINE)
+        match = VERSION_OPTION_REGEX.match(result.output.replace("\n", ""))
         assert match.group(1) == command_name
         assert match.group(2) == __version__
 
