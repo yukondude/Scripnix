bf807d46b9bce875177a79ada2b1fefdc33a987b
Dave Rogers <thedude@yukondude.com>
2016-10-19 21:41:59 -0700

    Add unit test for backup_file:collect_backups().

--- tests/test_backup_file.py
+++ tests/test_backup_file.py
@@ -4,9 +4,13 @@
 # This file is part of Scripnix. Copyright 2016 Dave Rogers <info@yukondude.com>. Licensed under the GNU General Public License, version 3.
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
+from click.testing import CliRunner
+import datetime
+import os
 # noinspection PyPackageRequirements
 import pytest
-from scripnix.pycommand.backup_file import assemble_dry_run_message, COMMAND_NAME, Backup, main
+import time
+from scripnix.pycommand.backup_file import assemble_dry_run_message, collect_backups, COMMAND_NAME, Backup, main
 from .common_options import common_help_option, common_version_option
 
 
@@ -29,3 +33,31 @@ def test_backup_file_assemble_dry_run_message(backups, expected):
 
 def test_backup_file_version_option():
     common_version_option(command_entry=main, command_name=COMMAND_NAME)
+
+
+@pytest.mark.parametrize('file_name_permissions,expected', [
+    ([], []),
+    ([("test.tst", 0o0644)], [Backup("test.tst", "test.tst.20140702", False)]),
+    ([("test.exe", 0o0755)], [Backup("test.exe", "test.exe.20140702", True)]),
+    ([("test.suid", 0o4644)], [Backup("test.suid", "test.suid.20140702", True)]),
+    ([("test.tst", 0o0644), ("test.exe", 0o0755)],
+     [Backup("test.tst", "test.tst.20140702", False), Backup("test.exe", "test.exe.20140702", True)]),
+    ([("test.tst", 0o0644), ("test.tst.20140702", 0o0644)],
+     [Backup("test.tst", "test.tst.20140702.1", False), Backup("test.tst.20140702", "test.tst.20140702.20140702", False)]),
+])
+def test_backup_file_collect_backups(file_name_permissions, expected):
+    with CliRunner().isolated_filesystem():
+        for file_name, mode in file_name_permissions:
+            with open(file_name, "a"):
+                os.utime(file_name, (time.time(), datetime.datetime(2014, 7, 2, 0, 0).timestamp()))
+            os.chmod(file_name, mode)
+
+        test_path = os.path.abspath(".")
+
+        backups = collect_backups([f[0] for f in file_name_permissions])
+        assert len(backups) == len(expected)
+
+        for i, backup in enumerate(backups):
+            assert backup.from_path == os.path.join(test_path, expected[i].from_path)
+            assert backup.to_path == os.path.join(test_path, expected[i].to_path)
+            assert backup.is_exec_or_suid == expected[i].is_exec_or_suid
