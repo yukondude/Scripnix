89e16c40b068c54a63145adbceecd4bec9f8f57e
Dave Rogers <thedude@yukondude.com>
2016-10-21 14:49:41 -0700

    Refactor all file writing into write_file() function.

--- scripnix/pycommand/install_scripnix.py
+++ scripnix/pycommand/install_scripnix.py
@@ -32,13 +32,10 @@ def install_global(execute, config_path):
     # Create /etc/scripnix/README
     readme_path = os.path.join(config_path, "README")
 
-    def write_readme():
-        with open(readme_path, "w", encoding="utf-8") as f:
-            f.write("Global Scripnix configuration settings.\n")
-            f.write("[https://github.com/yukondude/Scripnix]\n")
-
     if not os.path.isfile(readme_path):
-        execute(write_readme, echo="#create# {}".format(readme_path))
+        content = "Global Scripnix configuration settings.\n"
+        content += "[https://github.com/yukondude/Scripnix]\n"
+        execute(write_file, readme_path, content, echo="#create# {}".format(readme_path))
 
     execute(os.chmod, readme_path, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH, echo="chmod ugo=r {}".format(readme_path))
 
@@ -56,13 +53,10 @@ def install_global(execute, config_path):
     # Create /etc/scripnix/conf.bash
     conf_bash_path = os.path.join(config_path, "conf.bash")
 
-    def write_conf_bash():
-        with open(conf_bash_path, "w", encoding="utf-8") as f:
-            f.write("# Global configuration setting overrides for conf.bash.\n")
-            f.write("# User-specific setting overrides can be made in {}/conf.bash.\n".format(USER_CONFIG_DIR))
-
     if not os.path.isfile(conf_bash_path):
-        execute(write_conf_bash, echo="#create# {}".format(conf_bash_path))
+        content = "# Global configuration setting overrides for conf.bash.\n"
+        content += "# User-specific setting overrides can be made in {}/conf.bash.\n".format(USER_CONFIG_DIR)
+        execute(write_file, conf_bash_path, content, echo="#create# {}".format(conf_bash_path))
 
     execute(os.chmod, conf_bash_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH,
             echo="chmod u=rw,go=r {}".format(conf_bash_path))
@@ -71,27 +65,21 @@ def install_global(execute, config_path):
     sconf_bash_path = os.path.join(config_path, "sconf.bash")
     hostname = socket.gethostname().split('.')[0].lower()
 
-    def write_sconf_bash():
-        with open(sconf_bash_path, "w", encoding="utf-8") as f:
-            f.write("# Global configuration setting overrides for sconf.bash.\n")
-            f.write("# Root- or sudoer-specific setting overrides can be made in {}/sconf.bash.\n".format(USER_CONFIG_DIR))
-            f.write("MYSQL_DUMP_FILE='{}-pgsql-dump.tar'\n".format(hostname))
-            f.write("PGSQL_DUMP_FILE='{}-pgsql-dump.tar'\n".format(hostname))
-
     if not os.path.isfile(sconf_bash_path):
-        execute(write_sconf_bash, echo="#create# {}".format(sconf_bash_path))
+        content = "# Global configuration setting overrides for sconf.bash.\n"
+        content += "# Root- or sudoer-specific setting overrides can be made in {}/sconf.bash.\n".format(USER_CONFIG_DIR)
+        content += "MYSQL_DUMP_FILE='{}-pgsql-dump.tar'\n".format(hostname)
+        content += "PGSQL_DUMP_FILE='{}-pgsql-dump.tar'\n".format(hostname)
+        execute(write_file, sconf_bash_path, content, echo="#create# {}".format(sconf_bash_path))
 
     execute(os.chmod, sconf_bash_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP, echo="chmod u=rw,g=r,o= {}".format(sconf_bash_path))
 
     # Create /etc/scripnix/archive-exclusions
     archive_exclusions_path = os.path.join(config_path, "archive-exclusions")
 
-    def write_archive_exclusions():
-        with open(archive_exclusions_path, "w", encoding="utf-8") as f:
-            f.write("/var/archive\n")
-
     if not os.path.isfile(archive_exclusions_path):
-        execute(write_archive_exclusions, echo="#create# {}".format(archive_exclusions_path))
+        content = "/var/archive\n"
+        execute(write_file, archive_exclusions_path, content, echo="#create# {}".format(archive_exclusions_path))
 
     execute(os.chmod, archive_exclusions_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP,
             echo="chmod u=rw,g=r,o= {}".format(archive_exclusions_path))
@@ -151,3 +139,10 @@ def main(dry_run, verbose):
         install_global(execute, ROOT_CONFIG_DIR)
 
     # install_per_user(execute, USER_CONFIG_DIR)
+
+
+def write_file(path, content):
+    """ Open a new file with the given path and write the content into it.
+    """
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(content)
