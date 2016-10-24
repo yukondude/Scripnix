9e58499e44ff5181bdc80ae2c7fca98e7d004699
Dave Rogers <thedude@yukondude.com>
2016-10-23 22:32:19 -0700

    Add unit test for install_scripnix:install_per_user() and extract the file/dir permissions and file contents checks into their own functions.

--- tests/test_install_scripnix.py
+++ tests/test_install_scripnix.py
@@ -9,7 +9,36 @@ import os
 import re
 from scripnix import __version__
 from scripnix.pycommand.command import hostname, operating_system
-from scripnix.pycommand.install_scripnix import install_global
+from scripnix.pycommand.install_scripnix import COMMAND_NAME, install_global, install_per_user, main
+from .common_options import common_help_option, common_version_option
+
+
+def _check_exists_with_mode(*expected):
+    """ Test for expected files/directories with the correct permissions.
+    """
+    tree = []
+
+    for path, dir_names, file_names in os.walk("."):
+        tree += [os.path.join(path, d) for d in dir_names]
+        tree += [os.path.join(path, f) for f in file_names]
+
+    tree_attributes = [(t, os.stat(t).st_mode) for t in tree]
+
+    for name_mode in expected:
+        assert name_mode in tree_attributes
+
+
+def _check_file_contents(*expected):
+    """ Test for selected file contents matching a line-long regex.
+    """
+    for path, regex in expected:
+        with open(path, "r") as f:
+            whole_file = f.read()
+            assert re.match(regex, whole_file, re.MULTILINE) is not None
+
+
+def test_install_scripnix_help_option():
+    common_help_option(command_entry=main, command_name=COMMAND_NAME)
 
 
 def test_install_scripnix_install_global():
@@ -20,24 +49,14 @@ def test_install_scripnix_install_global():
     with CliRunner().isolated_filesystem():
         install_global(execute, config_path="./test", os_name=operating_system())
 
-        # Test for installed files and permissions.
-        tree = []
-
-        for path, dir_names, file_names in os.walk("."):
-            tree += [os.path.join(path, d) for d in dir_names]
-            tree += [os.path.join(path, f) for f in file_names]
-
-        tree_attributes = [(t, os.stat(t).st_mode) for t in tree]
-
-        expected = (("./test/archive-paths", 0o42750),
-                    ("./test/archive-exclusions", 0o100640),
-                    ("./test/conf.bash", 0o100644),
-                    ("./test/README", 0o100444),
-                    ("./test/sconf.bash", 0o100640),
-                    ("./test/scripnix-" + __version__, 0o100000))
-
-        for name_mode in expected:
-            assert name_mode in tree_attributes
+        #
+        _check_exists_with_mode(("./test", 0o42755),
+                                ("./test/archive-paths", 0o42750),
+                                ("./test/archive-exclusions", 0o100640),
+                                ("./test/conf.bash", 0o100644),
+                                ("./test/README", 0o100444),
+                                ("./test/sconf.bash", 0o100640),
+                                ("./test/scripnix-" + __version__, 0o100000))
 
         # Test for presence of archive-paths/ symlinks.
         archive_paths_path = "./test/archive-paths"
@@ -47,13 +66,25 @@ def test_install_scripnix_install_global():
             assert name.startswith(hostname())
             assert os.path.islink(os.path.join(archive_paths_path, name))
 
-        # Test for selected file contents (mostly that they're not empty).
-        expected = (("./test/archive-exclusions", r"^\/var\/archive$"),
-                    ("./test/conf.bash", r"^# Global configuration setting overrides for conf\.bash\.$"),
-                    ("./test/README", r"^Global Scripnix configuration settings\.$"),
-                    ("./test/sconf.bash", r"^# Global configuration setting overrides for sconf\.bash\.$"))
+        _check_file_contents(("./test/archive-exclusions", r"^\/var\/archive$"),
+                             ("./test/conf.bash", r"^# Global configuration setting overrides for conf\.bash\.$"),
+                             ("./test/README", r"^Global Scripnix configuration settings\.$"),
+                             ("./test/sconf.bash", r"^# Global configuration setting overrides for sconf\.bash\.$"))
 
-        for path, regex in expected:
-            with open(path, "r") as f:
-                whole_file = f.read()
-                assert re.match(regex, whole_file, re.MULTILINE)
+
+def test_install_scripnix_install_per_user():
+    def execute(fn, *args, echo):
+        fn(*args)
+        _ = echo
+
+    with CliRunner().isolated_filesystem():
+        install_per_user(execute, config_path="./.test")
+
+        _check_exists_with_mode(("./.test", 0o42750),
+                                ("./.test/README", 0o100440))
+
+        _check_file_contents(("./.test/README", r"^User Scripnix configuration settings\.$"))
+
+
+def test_install_scripnix_version_option():
+    common_version_option(command_entry=main, command_name=COMMAND_NAME)
