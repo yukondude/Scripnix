f3e36f3ae71298320b40366dc5375604ced866e4
Dave Rogers <thedude@yukondude.com>
2016-10-25 08:51:10 -0700

    OMFG, using 'setup.py develop' as part of the .travis.yml installation actually worked. Great Gosh A'Mighty.

--- tests/test_describe_scripnix.py
+++ tests/test_describe_scripnix.py
@@ -16,10 +16,6 @@ def test_help_option():
 
 
 def test_main():
-    # TODO: Skip testing the describe-scripnix command for the time being since it doesn't seem to work on Travis CI. Looks like the
-    # subprocess.check_output() call in collect_help_text() isn't finding the commands to run (~"/bin/sh backup-file not found").
-    # return
-
     result = CliRunner().invoke(main)
     help_text = result.output
     assert len(help_text) > 0
