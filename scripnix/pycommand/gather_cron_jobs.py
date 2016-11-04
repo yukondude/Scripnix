e01d00b89a737282064e815b7b600c4f084e365d
Dave Rogers <thedude@yukondude.com>
2016-11-04 09:52:08 -0700

    Great Renaming from 'rule' to 'job'.

--- scripnix/pycommand/gather_cron_jobs.py
+++ scripnix/pycommand/gather_cron_jobs.py
@@ -18,11 +18,11 @@ COMMAND_NAME = "gather-cron-jobs"
 # Any crontab comment or blank line.
 CRON_IGNORE_REGEX = re.compile(r"^(\s*#.*|\s*)$")
 
-CRON_RULE_PREFIX = r"^\s*"
-CRON_RULE_TERM = r"(\S+)\s+"
-CRON_RULE_SUFFIX = r"(.+?)\s*$"
-SYSTEM_CRON_RULE_REGEX = re.compile(CRON_RULE_PREFIX + CRON_RULE_TERM * 6 + CRON_RULE_SUFFIX)
-USER_CRON_RULE_REGEX = re.compile(CRON_RULE_PREFIX + CRON_RULE_TERM * 5 + CRON_RULE_SUFFIX)
+CRON_JOB_PREFIX = r"^\s*"
+CRON_JOB_TERM = r"(\S+)\s+"
+CRON_JOB_SUFFIX = r"(.+?)\s*$"
+SYSTEM_CRON_JOB_REGEX = re.compile(CRON_JOB_PREFIX + CRON_JOB_TERM * 6 + CRON_JOB_SUFFIX)
+USER_CRON_JOB_REGEX = re.compile(CRON_JOB_PREFIX + CRON_JOB_TERM * 5 + CRON_JOB_SUFFIX)
 
 CRON_SHORTCUTS = {
     '@annually': "0 0 1 1 *",
@@ -37,30 +37,30 @@ CRON_SHORTCUTS = {
 
 USER_CRON_CMD = "crontab -u {user} -l"
 
-# Structure for a single cron rule.
-CronRule = collections.namedtuple("CronRule", "minute hour day_of_the_month month day_of_the_week user command")
+# Structure for a single cron job.
+CronJob = collections.namedtuple("CronJob", "minute hour day_of_the_month month day_of_the_week user command")
 
-CRONTAB_HEADER = CronRule("m", "h", "dom", "mon", "dow", "user", "command")
+CRONTAB_HEADER = CronJob("m", "h", "dom", "mon", "dow", "user", "command")
 
 
-def format_cron_rules_table(cron_rules, header, delimiter, do_sort):
-    """ Return the given list of CronRule entries formatted as delimited lines of text, with an optional initial header line. Sort the lines
+def format_cron_jobs_table(cron_jobs, header, delimiter, do_sort):
+    """ Return the given list of CronJob entries formatted as delimited lines of text, with an optional initial header line. Sort the lines
         by the hour and minute elements if do_sort is True.
     """
     # Shallow-copy to avoid propagating in-place changes.
-    rules = cron_rules[:]
+    jobs = cron_jobs[:]
 
     if do_sort:
-        rules.sort(key=lambda e: (e.hour, e.minute))
+        jobs.sort(key=lambda e: (e.hour, e.minute))
 
     if header:
-        rules.insert(0, CRONTAB_HEADER)
+        jobs.insert(0, CRONTAB_HEADER)
 
-    return "\n".join([delimiter.join(c) for c in rules])
+    return "\n".join([delimiter.join(c) for c in jobs])
 
 
-def gather_single_user_cron_rules(user, do_unpack):
-    """ Return a list of parsed CronRule entries for the given user using the `crontab` shell command to do the work.
+def gather_single_user_cron_jobs(user, do_unpack):
+    """ Return a list of parsed CronJob entries for the given user using the `crontab` shell command to do the work.
     """
     try:
         crontab = subprocess.check_output([USER_CRON_CMD.format(user=user)], shell=True, stderr=subprocess.DEVNULL).decode('utf-8')
@@ -70,35 +70,35 @@ def gather_single_user_cron_rules(user, do_unpack):
     return parse_crontab(crontab, user, do_unpack)
 
 
-def gather_system_cron_rules(do_unpack):
+def gather_system_cron_jobs(do_unpack):
     # TODO: do the thing it's supposed to, you know, do.
     _ = do_unpack  # noqa
     return []
 
 
-def gather_user_cron_rules(users, do_unpack):
-    """ Return a list of gathered CronRule entries for all of the users given.
+def gather_user_cron_jobs(users, do_unpack):
+    """ Return a list of gathered CronJob entries for all of the users given.
     """
-    user_rules = [gather_single_user_cron_rules(user, do_unpack) for user in users]
-    return [rule for rules in user_rules for rule in rules]
+    user_jobs = [gather_single_user_cron_jobs(user, do_unpack) for user in users]
+    return [job for jobs in user_jobs for job in jobs]
 
 
-def parse_cron_rule(rule, user, do_unpack):
-    """ Parse a single line from a crontab file and return it as a list of CronRule namedtuples. If user is given, assume the line doesn't
+def parse_cron_job(job, user, do_unpack):
+    """ Parse a single line from a crontab file and return it as a list of CronJob namedtuples. If user is given, assume the line doesn't
         contain a user column. Return an empty list if the line is empty, a comment, or can't be parsed.
     """
-    if CRON_IGNORE_REGEX.match(rule) is not None:
+    if CRON_IGNORE_REGEX.match(job) is not None:
         return []
 
     # For simplicity's sake, replace '@' schedule shortcuts (except @reboot and @every_second which are ignored) with their corresponding
     # time fields.
     for shortcut in CRON_SHORTCUTS.keys():
-        if re.match(r"^\s*{}".format(shortcut), rule) is not None:
-            rule = rule.replace(shortcut, CRON_SHORTCUTS[shortcut])
+        if re.match(r"^\s*{}".format(shortcut), job) is not None:
+            job = job.replace(shortcut, CRON_SHORTCUTS[shortcut])
             break
 
-    crontab_regex = USER_CRON_RULE_REGEX if user else SYSTEM_CRON_RULE_REGEX
-    match = crontab_regex.match(rule)
+    crontab_regex = USER_CRON_JOB_REGEX if user else SYSTEM_CRON_JOB_REGEX
+    match = crontab_regex.match(job)
 
     if match is None:
         return []
@@ -107,15 +107,15 @@ def parse_cron_rule(rule, user, do_unpack):
     cron_command = match.group(6) if user else match.group(7)
 
     if not do_unpack:  # TODO: Actually unpack run-parts somewhere
-        return [CronRule(minute=match.group(1), hour=match.group(2), day_of_the_month=match.group(3), month=match.group(4),
-                         day_of_the_week=match.group(5), user=cron_user, command=cron_command)]
+        return [CronJob(minute=match.group(1), hour=match.group(2), day_of_the_month=match.group(3), month=match.group(4),
+                        day_of_the_week=match.group(5), user=cron_user, command=cron_command)]
 
 
 def parse_crontab(crontab, user, do_unpack):
-    """ Return a list of parsed, non-empty, CronRule entries for the given crontab text file contents.
+    """ Return a list of parsed, non-empty, CronJob entries for the given crontab text file contents.
     """
-    parsed_rules = [parse_cron_rule(rule, user, do_unpack) for rule in crontab.split("\n")]
-    return list(filter(None, [rule for rules in parsed_rules for rule in rules]))
+    parsed_jobs = [parse_cron_job(job, user, do_unpack) for job in crontab.split("\n")]
+    return list(filter(None, [job for jobs in parsed_jobs for job in jobs]))
 
 
 @common_command_and_options(command_name=COMMAND_NAME)
@@ -136,6 +136,6 @@ def main(delimiter, no_header, run_parts, sort):
     check_root_user(command_name=COMMAND_NAME)
     _ = read_configuration()  # noqa
 
-    consolidated_cron_rules = gather_system_cron_rules(run_parts)
-    consolidated_cron_rules.extend(gather_user_cron_rules(users=[p.pw_name for p in pwd.getpwall()], do_unpack=run_parts))
-    click.echo(format_cron_rules_table(consolidated_cron_rules, not no_header, "\t" if not delimiter else delimiter, sort))
+    consolidated_cron_jobs = gather_system_cron_jobs(run_parts)
+    consolidated_cron_jobs.extend(gather_user_cron_jobs(users=[p.pw_name for p in pwd.getpwall()], do_unpack=run_parts))
+    click.echo(format_cron_jobs_table(consolidated_cron_jobs, not no_header, "\t" if not delimiter else delimiter, sort))
--- tests/test_gather_cron_jobs.py
+++ tests/test_gather_cron_jobs.py
@@ -6,7 +6,7 @@
 
 # noinspection PyPackageRequirements
 import pytest
-from scripnix.pycommand.gather_cron_jobs import COMMAND_NAME, CronRule, format_cron_rules_table, main, parse_cron_rule, parse_crontab
+from scripnix.pycommand.gather_cron_jobs import COMMAND_NAME, CronJob, format_cron_jobs_table, main, parse_cron_job, parse_crontab
 from .command import common_help_option, common_version_option
 
 
@@ -30,11 +30,11 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 47 0	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )
 #"""
 
-SAMPLE_SYSTEM_CRON_RULES = [
-    CronRule(*line2args("07 * * * * root"), command="cd / && run-parts --report /etc/cron.hourly"),
-    CronRule(*line2args("17 0 * * * root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )"),
-    CronRule(*line2args("37 0 * * 7 root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )"),
-    CronRule(*line2args("47 0 1 * * root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )"),
+SAMPLE_SYSTEM_CRON_JOBS = [
+    CronJob(*line2args("07 * * * * root"), command="cd / && run-parts --report /etc/cron.hourly"),
+    CronJob(*line2args("17 0 * * * root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )"),
+    CronJob(*line2args("37 0 * * 7 root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )"),
+    CronJob(*line2args("47 0 1 * * root"), command="test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )"),
 ]
 
 SAMPLE_USER_CRONTAB = """MAIL:
@@ -51,13 +51,13 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/home/me
 
 """
 
-SAMPLE_USER_CRON_RULES = [
-    CronRule(*line2args("0 0 * * *"), user="user3", command="/usr/bin/wget -O - -q -t 1 http://localhost/cron.php"),
-    CronRule(*line2args("* * * * *"), user="user3", command="/bin/all-the-time --you-bet 2>/dev/null"),
-    CronRule(*line2args("5 10,22 * * *"), user="user3", command="/home/me/do-it-daily --yes -Q"),
-    CronRule(*line2args("30 4 * * *"), user="user3", command="/bin/backup"),
-    CronRule(*line2args("5 0,4,10,16 * * *"), user="user3", command="/bin/now --and --again >/tmp/log"),
-    CronRule(*line2args("0 0 * * *"), user="user3", command="nightly-routine --need >doing"),
+SAMPLE_USER_CRON_JOBS = [
+    CronJob(*line2args("0 0 * * *"), user="user3", command="/usr/bin/wget -O - -q -t 1 http://localhost/cron.php"),
+    CronJob(*line2args("* * * * *"), user="user3", command="/bin/all-the-time --you-bet 2>/dev/null"),
+    CronJob(*line2args("5 10,22 * * *"), user="user3", command="/home/me/do-it-daily --yes -Q"),
+    CronJob(*line2args("30 4 * * *"), user="user3", command="/bin/backup"),
+    CronJob(*line2args("5 0,4,10,16 * * *"), user="user3", command="/bin/now --and --again >/tmp/log"),
+    CronJob(*line2args("0 0 * * *"), user="user3", command="nightly-routine --need >doing"),
 ]
 
 SAMPLE_USER_CRON_TABLE = """m	h	dom	mon	dow	user	command
@@ -83,10 +83,10 @@ SAMPLE_USER_CRON_TABLE = """m	h	dom	mon	dow	user	command
     (False, "@@", False),
     (False, " ", False),
 ])
-def test_format_cron_rules_table(header, delimiter, do_sort):
-    sample_user_cron_rules_copy = SAMPLE_USER_CRON_RULES[:]
-    formatted = format_cron_rules_table(SAMPLE_USER_CRON_RULES, header, delimiter, do_sort)
-    assert sample_user_cron_rules_copy == SAMPLE_USER_CRON_RULES
+def test_format_cron_jobs_table(header, delimiter, do_sort):
+    sample_user_cron_jobs_copy = SAMPLE_USER_CRON_JOBS[:]
+    formatted = format_cron_jobs_table(SAMPLE_USER_CRON_JOBS, header, delimiter, do_sort)
+    assert sample_user_cron_jobs_copy == SAMPLE_USER_CRON_JOBS
     expected = SAMPLE_USER_CRON_TABLE.replace("\t", delimiter) if delimiter != "\t" else SAMPLE_USER_CRON_TABLE
 
     if not header:
@@ -104,7 +104,7 @@ def test_help_option():
     common_help_option(command_entry=main, command_name=COMMAND_NAME)
 
 
-@pytest.mark.parametrize('rule,user,do_unpack,expected', [
+@pytest.mark.parametrize('job,user,do_unpack,expected', [
     ("", None, False, []),
     ("  # Comment", None, False, []),
     ("* * * * false >/dev/null", None, False, []),
@@ -113,43 +113,43 @@ def test_help_option():
     ("  # Comment", "user2", False, []),
     ("* * * false >/dev/null", "user2", False, []),
     ("PATH=/usr/local/bin:/home/user1/bin", "user2", False, []),
-    ("* * * * * /bin/all -the >time", "user1", False, [CronRule(*line2args("* * * * * user1"), command="/bin/all -the >time")]),
-    ("*    *     *  * *   /bin/every-minute", "user1", False, [CronRule(*line2args("* * * * * user1 /bin/every-minute"))]),
-    ("*/10 *     *  * *   /bin/every-10-minutes", "user1", False, [CronRule(*line2args("*/10 * * * * user1 /bin/every-10-minutes"))]),
-    ("35   0     16 6 *   /bin/june-16", "user1", False, [CronRule(*line2args("35 0 16 6 * user1 /bin/june-16"))]),
-    ("00   11,16 *  * *   /bin/twice-per-day", "user1", False, [CronRule(*line2args("00 11,16 * * * user1 /bin/twice-per-day"))]),
-    ("00   09-17 *  * *   /bin/work-hours", "user1", False, [CronRule(*line2args("00 09-17 * * * user1 /bin/work-hours"))]),
-    ("00   13    *  * 1-5 /bin/weekdays", "user1", False, [CronRule(*line2args("00 13 * * 1-5 user1 /bin/weekdays"))]),
-    ("00   14    1  1 *   /bin/january-1", "user1", False, [CronRule(*line2args("00 14 1 1 * user1 /bin/january-1"))]),
+    ("* * * * * /bin/all -the >time", "user1", False, [CronJob(*line2args("* * * * * user1"), command="/bin/all -the >time")]),
+    ("*    *     *  * *   /bin/every-minute", "user1", False, [CronJob(*line2args("* * * * * user1 /bin/every-minute"))]),
+    ("*/10 *     *  * *   /bin/every-10-minutes", "user1", False, [CronJob(*line2args("*/10 * * * * user1 /bin/every-10-minutes"))]),
+    ("35   0     16 6 *   /bin/june-16", "user1", False, [CronJob(*line2args("35 0 16 6 * user1 /bin/june-16"))]),
+    ("00   11,16 *  * *   /bin/twice-per-day", "user1", False, [CronJob(*line2args("00 11,16 * * * user1 /bin/twice-per-day"))]),
+    ("00   09-17 *  * *   /bin/work-hours", "user1", False, [CronJob(*line2args("00 09-17 * * * user1 /bin/work-hours"))]),
+    ("00   13    *  * 1-5 /bin/weekdays", "user1", False, [CronJob(*line2args("00 13 * * 1-5 user1 /bin/weekdays"))]),
+    ("00   14    1  1 *   /bin/january-1", "user1", False, [CronJob(*line2args("00 14 1 1 * user1 /bin/january-1"))]),
     ("      @reboot /bin/reboot", "user1", False, []),
-    ("@every_minute /bin/every-minute", "user1", False, [CronRule(*line2args("* * * * * user1 /bin/every-minute"))]),
-    ("      @weekly /bin/weekly", "user1", False, [CronRule(*line2args("0 0 * * 0 user1 /bin/weekly"))]),
-    ("     @monthly /bin/monthly", "user1", False, [CronRule(*line2args("0 0 1 * * user1 /bin/monthly"))]),
-    ("      @yearly /bin/yearly", "user1", False, [CronRule(*line2args("0 0 1 1 * user1 /bin/yearly"))]),
-    ("*    *     *  * *   user2 /bin/all -the >time", None, False, [CronRule(*line2args("* * * * * user2"),
-                                                                             command="/bin/all -the >time")]),
-    ("*    *     *  * *   user2 /bin/every-minute", None, False, [CronRule(*line2args("* * * * * user2 /bin/every-minute"))]),
-    ("*/10 *     *  * *   user2 /bin/every-10-minutes", None, False, [CronRule(*line2args("*/10 * * * * user2 /bin/every-10-minutes"))]),
-    ("35   0     16 6 *   user2 /bin/june-16", None, False, [CronRule(*line2args("35 0 16 6 * user2 /bin/june-16"))]),
-    ("00   11,16 *  * *   user2 /bin/twice-per-day", None, False, [CronRule(*line2args("00 11,16 * * * user2 /bin/twice-per-day"))]),
-    ("00   09-17 *  * *   user2 /bin/work-hours", None, False, [CronRule(*line2args("00 09-17 * * * user2 /bin/work-hours"))]),
-    ("00   13    *  * 1-5 user2 /bin/weekdays", None, False, [CronRule(*line2args("00 13 * * 1-5 user2 /bin/weekdays"))]),
-    ("00   14    1  1 *   user2 /bin/january-1", None, False, [CronRule(*line2args("00 14 1 1 * user2 /bin/january-1"))]),
+    ("@every_minute /bin/every-minute", "user1", False, [CronJob(*line2args("* * * * * user1 /bin/every-minute"))]),
+    ("      @weekly /bin/weekly", "user1", False, [CronJob(*line2args("0 0 * * 0 user1 /bin/weekly"))]),
+    ("     @monthly /bin/monthly", "user1", False, [CronJob(*line2args("0 0 1 * * user1 /bin/monthly"))]),
+    ("      @yearly /bin/yearly", "user1", False, [CronJob(*line2args("0 0 1 1 * user1 /bin/yearly"))]),
+    ("*    *     *  * *   user2 /bin/all -the >time", None, False, [CronJob(*line2args("* * * * * user2"),
+                                                                            command="/bin/all -the >time")]),
+    ("*    *     *  * *   user2 /bin/every-minute", None, False, [CronJob(*line2args("* * * * * user2 /bin/every-minute"))]),
+    ("*/10 *     *  * *   user2 /bin/every-10-minutes", None, False, [CronJob(*line2args("*/10 * * * * user2 /bin/every-10-minutes"))]),
+    ("35   0     16 6 *   user2 /bin/june-16", None, False, [CronJob(*line2args("35 0 16 6 * user2 /bin/june-16"))]),
+    ("00   11,16 *  * *   user2 /bin/twice-per-day", None, False, [CronJob(*line2args("00 11,16 * * * user2 /bin/twice-per-day"))]),
+    ("00   09-17 *  * *   user2 /bin/work-hours", None, False, [CronJob(*line2args("00 09-17 * * * user2 /bin/work-hours"))]),
+    ("00   13    *  * 1-5 user2 /bin/weekdays", None, False, [CronJob(*line2args("00 13 * * 1-5 user2 /bin/weekdays"))]),
+    ("00   14    1  1 *   user2 /bin/january-1", None, False, [CronJob(*line2args("00 14 1 1 * user2 /bin/january-1"))]),
     ("      @reboot user2 /bin/reboot", None, False, []),
-    ("@every_minute user2 /bin/every-minute", None, False, [CronRule(*line2args("* * * * * user2 /bin/every-minute"))]),
-    ("      @weekly user2 /bin/weekly", None, False, [CronRule(*line2args("0 0 * * 0 user2 /bin/weekly"))]),
-    ("     @monthly user2 /bin/monthly", None, False, [CronRule(*line2args("0 0 1 * * user2 /bin/monthly"))]),
-    ("      @yearly user2 /bin/yearly", None, False, [CronRule(*line2args("0 0 1 1 * user2 /bin/yearly"))]),
+    ("@every_minute user2 /bin/every-minute", None, False, [CronJob(*line2args("* * * * * user2 /bin/every-minute"))]),
+    ("      @weekly user2 /bin/weekly", None, False, [CronJob(*line2args("0 0 * * 0 user2 /bin/weekly"))]),
+    ("     @monthly user2 /bin/monthly", None, False, [CronJob(*line2args("0 0 1 * * user2 /bin/monthly"))]),
+    ("      @yearly user2 /bin/yearly", None, False, [CronJob(*line2args("0 0 1 1 * user2 /bin/yearly"))]),
 ])
-def test_parse_cron_rule(rule, user, do_unpack, expected):
-    assert parse_cron_rule(rule, user, do_unpack) == expected
+def test_parse_cron_job(job, user, do_unpack, expected):
+    assert parse_cron_job(job, user, do_unpack) == expected
 
 
 @pytest.mark.parametrize('crontab,user,do_unpack,expected', [
     ("", None, False, []),
     ("", "user3", False, []),
-    (SAMPLE_SYSTEM_CRONTAB, None, False, SAMPLE_SYSTEM_CRON_RULES),
-    (SAMPLE_USER_CRONTAB, "user3", False, SAMPLE_USER_CRON_RULES),
+    (SAMPLE_SYSTEM_CRONTAB, None, False, SAMPLE_SYSTEM_CRON_JOBS),
+    (SAMPLE_USER_CRONTAB, "user3", False, SAMPLE_USER_CRON_JOBS),
 ])
 def test_parse_crontab(crontab, user, do_unpack, expected):
     assert parse_crontab(crontab, user, do_unpack) == expected
